0bd2d83ab09faad79fd930626dc9dc975504fc39
mcoding777 <mcoding777@naver.com>
2025-07-06 23:27:22 +0900

    refactor: 문답 페이지 네비게이션 버튼 컴포넌트 분리리

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -8,6 +8,7 @@ import Spinner from "@/components/etc/Spinner";
 import { useTestStore } from "@/hooks/useTestStore";
 import { Question } from "@/types/test";
 import { useEffect, useState } from "react";
+import NaviButtons from "@/components/test/NaviButtons";
 
 export default function Test() {
     const [questions, setQuestions] = useState<Question[]>([]);
@@ -16,18 +17,6 @@ export default function Test() {
     const answersLen = useTestStore((state) => state.answers.size)
 
     const setAnswer = useTestStore((state) => state.setAnswer)
-
-    const handleNext = () => {
-        if (currentQa < questions.length - 1) {
-            setCurrentQa(prev => prev + 1);
-        }
-    };
-
-    const handlePrev = () => {
-        if (currentQa > 0) {
-            setCurrentQa(prev => prev - 1);
-        }
-    };
 
     useEffect(() => {
         fetch("/data/question.json")
@@ -48,39 +37,7 @@ export default function Test() {
             <QA idx={currentQa} currentQ={questions[currentQa]} value={currentValue} setValue={setAnswer} />
 
             {/* 네비게이션 버튼 */}
-            <div className="flex justify-between items-center">
-                <button
-                    onClick={handlePrev}
-                    disabled={currentQa === 0}
-                    className={`
-flex items-center px-4 py-2 rounded-lg font-medium transition-all duration-200
-${currentQa === 0
-                            ? 'text-gray-400 cursor-not-allowed'
-                            : 'text-gray-600 hover:text-gray-800 hover:bg-gray-100'
-                        }
-`}
-                >
-                    <ChevronLeft className="w-4 h-4 mr-1" />
-                    이전
-                </button>
-
-                <button
-                    onClick={handleNext}
-                    disabled={!currentValue || currentQa === questions.length - 1}
-                    className={`
-flex items-center px-6 py-3 rounded-lg font-medium transition-all duration-200
-${!currentValue || currentQa === questions.length - 1
-                            ? 'bg-gray-200 text-gray-400 cursor-not-allowed'
-                            : 'bg-gradient-to-r from-blue-500 to-indigo-600 text-white hover:from-blue-600 hover:to-indigo-700 shadow-lg hover:shadow-xl'
-                        }
-`}
-                >
-                    {currentQa === questions.length - 1 ? '결과보기' : '다음'}
-                    {currentQa !== questions.length - 1 && (
-                        <ChevronRight className="w-4 h-4 ml-1" />
-                    )}
-                </button>
-            </div>
+            <NaviButtons currentQa={currentQa} setCurrentQa={setCurrentQa} max={questions.length - 1} min={0} />
         </>
     )
 }
---
+++ src/components/test/NaviButtons.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { Dispatch, SetStateAction } from "react";
+import ChevronLeft from "./ChevronLeft";
+import { useTestStore } from "@/hooks/useTestStore";
+import ChevronRight from "./ChevronRight";
+
+interface NaviButtonsProps {
+    currentQa: number
+    setCurrentQa: Dispatch<SetStateAction<number>>
+    max: number
+    min: number
+}
+
+export default function NaviButtons({ currentQa, setCurrentQa, max, min }: NaviButtonsProps) {
+    const currentValue = useTestStore((state) => state.answers.get(currentQa + 1) ?? null)
+
+    const handleNext = () => {
+        setCurrentQa(prev => {
+            if (prev < max) {
+                return prev + 1
+            }
+            return prev
+        });
+    };
+
+    const handlePrev = () => {
+        setCurrentQa(prev => {
+            if (currentQa > min) {
+                return prev - 1
+            }
+            return prev
+        });
+    };
+
+    return <div className="flex justify-between items-center">
+        <button
+            onClick={handlePrev}
+            disabled={currentQa === 0}
+            className={`
+flex items-center px-4 py-2 rounded-lg font-medium transition-all duration-200
+${currentQa === 0
+                    ? 'text-gray-400 cursor-not-allowed'
+                    : 'text-gray-600 hover:text-gray-800 hover:bg-gray-100'
+                }
+`}
+        >
+            <ChevronLeft className="w-4 h-4 mr-1" />
+            이전
+        </button>
+
+        <button
+            onClick={handleNext}
+            disabled={!currentValue || currentQa === max}
+            className={`
+flex items-center px-6 py-3 rounded-lg font-medium transition-all duration-200
+${!currentValue || currentQa === max
+                    ? 'bg-gray-200 text-gray-400 cursor-not-allowed'
+                    : 'bg-gradient-to-r from-blue-500 to-indigo-600 text-white hover:from-blue-600 hover:to-indigo-700 shadow-lg hover:shadow-xl'
+                }
+`}
+        >
+            {currentQa === max ? '결과보기' : '다음'}
+            {currentQa !== max && (
+                <ChevronRight className="w-4 h-4 ml-1" />
+            )}
+        </button>
+    </div>
+}
